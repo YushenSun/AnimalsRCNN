98b60710a477611e0c9d3384e16378dd9e20a16a
Yushen Sun <sun.yushen@gmail.com>
2023-08-13 02:45:38 +0100

    train loop(8-3 channels conflict)

--- RCNN.py
+++ RCNN.py
@@ -1,10 +1,10 @@
 import torchvision.transforms as transforms
 from osgeo import gdal
 import os
-import gdal
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
+from PIL import Image
 
 # Define the list of target class labels
 classes = ['elephant', 'lion', 'giraffe', 'zebra', 'rhino', 'non-animal']
@@ -15,12 +15,6 @@ class_to_idx = {cls: idx for idx, cls in enumerate(classes)}
 # Define the path to your dataset and annotation file
 data_dir = 'D:/RS/Blocks_20SEP'
 annotation_file = 'D:/RS/ano/20SEP.csv'  # Replace with the actual annotation file path
-
-# Define a transform to preprocess the input image (if needed)
-transform = transforms.Compose([
-    # Add your transformations here if needed
-])
-
 
 # CustomDataset class
 class CustomDataset(Dataset):
@@ -38,12 +32,10 @@ class CustomDataset(Dataset):
             for line in lines[1:]:  # Skip the header line
                 line = line.strip().split(',')
                 image_name = line[0]
-                boxes = []
                 category = line[1]
                 bbox = [float(coord) for coord in line[2:]]
-                boxes.append({'category': category, 'bbox': bbox})
-                annotations.append({'image': image_name, 'boxes': boxes})
-        return annotations
+                annotations.append({'image': image_name, 'boxes': [bbox], 'category': [category]})
+            return annotations
 
     def __getitem__(self, idx):
         annotation = self.annotations[idx]
@@ -57,10 +49,9 @@ class CustomDataset(Dataset):
         # Convert to torch tensor and specify the data type
         image = torch.from_numpy(image.astype(np.float32))  # or np.float64
 
-        num_boxes = len(annotation['boxes'])
         target = {
-            'boxes': torch.tensor([box['bbox'] for box in annotation['boxes']], dtype=torch.float32),
-            'labels': torch.tensor([self.class_to_idx[box['category']] for box in annotation['boxes']]),
+            'boxes': torch.tensor(annotation['boxes'], dtype=torch.float32),
+            'labels': torch.tensor([self.class_to_idx[cat] for cat in annotation['category']]),
         }
 
         return image, target
@@ -68,8 +59,40 @@ class CustomDataset(Dataset):
     def __len__(self):
         return len(self.annotations)
 
+# Define a transform to preprocess the input image (if needed)
+class ChannelwiseNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = torch.tensor(mean, dtype=torch.float32)
+        self.std = torch.tensor(std, dtype=torch.float32)
+
+    def __call__(self, image):
+        num_channels = image.size(0)  # Get the number of channels in the image
+        mean = self.mean[:num_channels]  # Use only the required mean values
+        std = self.std[:num_channels]    # Use only the required std values
+        image = (image - mean.view(num_channels, 1, 1)) / std.view(num_channels, 1, 1)
+        return image
+
 
 # Instantiate the CustomDataset
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Convert PIL Image to PyTorch tensor
+    ChannelwiseNormalize(mean=[262, 264, 354, 376, 262, 443, 502, 469],
+                         std=[13, 15, 24, 29, 23, 40, 48, 49]),
+])
+
+
+# Instantiate the CustomDataset
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Convert PIL Image to PyTorch tensor
+])
+
+# Print the shape of mean and std
+#print("Mean shape:", np.array([262]).shape)
+#print("Std shape:", np.array([13]).shape)
+
+custom_dataset = CustomDataset(data_dir, annotation_file, transform)
+
+
 custom_dataset = CustomDataset(data_dir, annotation_file, transform)
 
 # Create a DataLoader for training
@@ -81,9 +104,84 @@ train_loader = DataLoader(
     collate_fn=lambda x: list(zip(*x))  # This collates the data into batches
 )
 
-# Iterate through the train_loader
-for batch in train_loader:
-    images, targets = batch  # Extract the images and targets from the batch tuple
-    images = torch.stack(images)  # Convert list of images to a tensor
-    # Forward pass, loss calculation, and backpropagation here
-    # Implement your training loop logic
+# ... (rest of the code for the model, training loop, etc.)
+# ... (previous code)
+
+# Define the object detection model
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+# Define the backbone model for Faster R-CNN
+backbone = torchvision.models.detection.backbone_utils.resnet_fpn_backbone('resnet50', pretrained=True)
+
+# Define the anchor generator
+rpn_anchor_generator = AnchorGenerator(
+    sizes=((32, 64, 128, 256, 512),),
+    aspect_ratios=((0.5, 1.0, 2.0),) * 5
+)
+
+# Create the Faster R-CNN model
+model = FasterRCNN(
+    backbone,
+    num_classes=len(classes),  # Number of target classes
+    rpn_anchor_generator=rpn_anchor_generator
+)
+
+# Training loop
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+
+# Define optimizer and learning rate scheduler
+optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
+lr_scheduler = StepLR(optimizer, step_size=3, gamma=0.1)
+
+# Set the model in training mode
+model.train()
+
+# Number of training epochs
+num_epochs = 10
+
+# Define the device (GPU if available, otherwise CPU)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# Training loop
+for epoch in range(num_epochs):
+    for batch_idx, batch in enumerate(train_loader):
+        images, targets = batch
+
+        # Transfer images and targets to the device
+        images = [img.to(device) for img in images]
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        # Modify the dimensions of the target boxes to match the number of channels in images
+        for t in targets:
+            if 'boxes' in t:
+                t['boxes'][:, [0, 2]] *= 8  # Adjust x_min and x_max coordinates
+                t['boxes'][:, [1, 3]] *= 8  # Adjust y_min and y_max coordinates
+
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Print shapes of images and targets for debugging
+        print(f"Epoch [{epoch+1}/{num_epochs}] - Batch [{batch_idx+1}/{len(train_loader)}]")
+        print("Images shape:", images[0].shape)  # Print shape of the first image in the batch
+        print("Targets boxes shape:", targets[0]['boxes'].shape)  # Print shape of boxes in the first target
+        print("Targets labels shape:", targets[0]['labels'].shape)  # Print shape of labels in the first target
+
+        # Forward pass
+        loss_dict = model(images, targets)
+
+        # Calculate total loss
+        total_loss = sum(loss for loss in loss_dict.values())
+
+        # Backpropagation
+        total_loss.backward()
+        optimizer.step()
+
+    # Update the learning rate
+    lr_scheduler.step()
+
+    print(f"Epoch [{epoch+1}/{num_epochs}] - Total Loss: {total_loss.item()}")
+
+# ... (continue with validation, evaluation, inference, and saving the model)
